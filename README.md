2d156271307ad6886f1c6e61564dcc2b7f9e7f2b
Conor Casey <callmeconor@gmail.com>
2022-12-06 21:55:46 -0800

    update README and modify verbiage for day 5 notes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+This is a repository containing my solutions for the 2022 Advent of Code challenges.
+
 [Link to Advent of Code](https://adventofcode.com/)
+
+Each folder contains a solution for the corresponding day of the challenge.
--- day5/day5.py
+++ day5/day5.py
@@ -4,5 +4,5 @@ for current_line in input:
     current_line = current_line.strip()
     print(current_line)
 
-# must implement 9 stacks, one for each 'stack'
+# must implement 9 stacks, one for each 'stack' of crates
 # pop x number off current stack and push those values onto new stack
