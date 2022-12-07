0bc6652777e118d54ff6800323ff5b0496efa92c
Conor Casey <callmeconor@gmail.com>
2022-12-06 21:50:36 -0800

    add initial code for day 5 solution

---
+++ day5/day5.py
@@ -0,0 +1,8 @@
+input = open("input.txt","r")
+
+for current_line in input:
+    current_line = current_line.strip()
+    print(current_line)
+
+# must implement 9 stacks, one for each 'stack'
+# pop x number off current stack and push those values onto new stack
