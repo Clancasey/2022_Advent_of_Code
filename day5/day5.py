1b0504f5ecaf6857f5ca4790dc63781d738a8758
Conor Casey <callmeconor@gmail.com>
2022-12-09 15:02:51 -0800

    add day 5 part 1 solution

--- day5/day5.py
+++ day5/day5.py
@@ -1,8 +1,29 @@
+from collections import deque
+
+
 input = open("input.txt","r")
+full_input = input.readlines()
+stacks = [deque() for _ in range(10)]
 
-for current_line in input:
-    current_line = current_line.strip()
-    print(current_line)
+for i in range(7,-1,-1):
+    for j in range(1,10,1):
+        current_letter = full_input[i][4*j-3]
+        if ord(current_letter) in range(65,91):
+            stacks[j].append(current_letter)
+print(stacks)
 
-# must implement 9 stacks, one for each 'stack' of crates
-# pop x number off current stack and push those values onto new stack
+for i in range(10,len(full_input)):
+    current_line=full_input[i].strip()
+    split_line = current_line.split(' ')
+    
+    crate_number = int(split_line[1])
+    source_stack = int(split_line[3])
+    destination_stack = int(split_line[5])
+    for j in range(crate_number):
+        stacks[destination_stack].append(stacks[source_stack].pop())
+
+top_string = ''
+for i in range(1,10,1):
+    top_string+=stacks[i].pop()
+
+print(top_string)
