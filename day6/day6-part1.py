496647d755d93a90d8cd1aa6087156691ddafef3
Conor Casey <callmeconor@gmail.com>
2022-12-09 15:46:25 -0800

    add day 6 solutions

---
+++ day6/day6-part1.py
@@ -0,0 +1,15 @@
+input = open("input.txt","r").readline()
+
+marker_list = []
+for i in range(len(input)):
+    for marker in marker_list:
+        if ord(input[i]) == marker:
+            marker_list = []
+            break
+
+    marker_list.append(ord(input[i]))
+    if len(marker_list) == 4:
+        solution = i+1
+        break
+
+print("Part 1 solution: " + str(solution) + ", with marker: " + input[solution-4:solution])
