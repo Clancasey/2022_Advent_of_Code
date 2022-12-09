b58a685b7f7c8a16f78c37c9c5555f2243ea3657
Conor Casey <callmeconor@gmail.com>
2022-12-09 15:16:46 -0800

    add day 5 part 2 solution

--- day5/day5.py
+++ day5/day5.py
@@ -1,4 +1,5 @@
 from collections import deque
+import copy
 
 
 input = open("input.txt","r")
@@ -10,7 +11,8 @@ for i in range(7,-1,-1):
         current_letter = full_input[i][4*j-3]
         if ord(current_letter) in range(65,91):
             stacks[j].append(current_letter)
-print(stacks)
+
+stacks_p2 = copy.deepcopy(stacks)
 
 for i in range(10,len(full_input)):
     current_line=full_input[i].strip()
@@ -19,11 +21,21 @@ for i in range(10,len(full_input)):
     crate_number = int(split_line[1])
     source_stack = int(split_line[3])
     destination_stack = int(split_line[5])
+    
+    temp_stack = deque()
     for j in range(crate_number):
         stacks[destination_stack].append(stacks[source_stack].pop())
+        temp_stack.append(stacks_p2[source_stack].pop())
+    
+    for j in range(crate_number):
+        stacks_p2[destination_stack].append(temp_stack.pop())
 
-top_string = ''
+top_string, top_string_p2 = '', ''
+
 for i in range(1,10,1):
     top_string+=stacks[i].pop()
+    top_string_p2+=stacks_p2[i].pop()
 
-print(top_string)
+print("Part 1 solution: " + top_string)
+print("Part 2 solution: " + top_string_p2)
+
