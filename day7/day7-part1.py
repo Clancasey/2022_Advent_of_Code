1e9bf8c54834a9424d0ab7a17b569ad6fea32b2d
Conor Casey <callmeconor@gmail.com>
2022-12-09 16:18:14 -0800

    implement initial structure for day 7 p1 solution

---
+++ day7/day7-part1.py
@@ -0,0 +1,11 @@
+input = open("input.txt","r")
+
+for line in input:
+    line = line.strip()
+    print(line)
+    if line[0] == '$': # Detected a command
+        print('detected a command')
+    if 48 <= ord(line[0]) <= 57: # Detected a file
+        print('detected a file')
+    if line[0:3] == 'dir': # Detected a directory
+        print('detected a directory')
