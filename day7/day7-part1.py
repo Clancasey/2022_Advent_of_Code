12e530c794300f1b113d82fcc082eb2a8b752e40
Conor Casey <Conor.C@eviation.co>
2022-12-12 12:28:04 -0800

    remove unnecessary print lines from day 7 solution

--- day7/day7-part1.py
+++ day7/day7-part1.py
@@ -4,8 +4,8 @@ for line in input:
     line = line.strip()
     print(line)
     if line[0] == '$': # Detected a command
-        print('detected a command')
+        # print('detected a command')
     if 48 <= ord(line[0]) <= 57: # Detected a file
-        print('detected a file')
+        # print('detected a file')
     if line[0:3] == 'dir': # Detected a directory
-        print('detected a directory')
+        # print('detected a directory')
